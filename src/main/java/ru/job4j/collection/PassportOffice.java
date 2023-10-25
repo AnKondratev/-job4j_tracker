cea6e80e75f2922bf59256748878ee7dc4efc5d9
AnKondratev <kondratev.an@inbox.ru>
2023-10-25 14:30:11 +0400

    Паспорт и жители

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+        }
+        return !citizens.containsKey(citizen.getPassport());
+    }
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}

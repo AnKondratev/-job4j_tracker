4e5f4313db05ad1d40eb5f5a499b2c2211748abb
AnKondratev <kondratev.an@inbox.ru>
2023-11-02 19:01:10 +0400

    Сортировка

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemAscByNameTest {
+
+    @Test
+    void whenListSorted() {
+        List<Item> items = new ArrayList<>(List.of(new Item("Ben"), new Item("Zack"), new Item("Adam"), new Item("Deny")));
+        List<Item> expected = new ArrayList<>(List.of(new Item("Adam"), new Item("Ben"), new Item("Deny"), new Item("Zack")));
+        items.sort(new ItemAscByName());
+        assertThat(items.equals(expected)).isTrue();
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemDescByNameTest {
+
+    @Test
+    void whenListSorted() {
+        List<Item> items = new ArrayList<>(List.of(new Item("Ben"), new Item("Zack"), new Item("Adam"), new Item("Deny")));
+        List<Item> expected = new ArrayList<>(List.of(new Item("Zack"), new Item("Deny"), new Item("Ben"), new Item("Adam")));
+        items.sort(new ItemDescByName());
+        assertThat(items.equals(expected)).isTrue();
+    }
+}

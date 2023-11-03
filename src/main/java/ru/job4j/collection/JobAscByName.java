77fd094aedd274c11b6180041280ff3f390971e8
AnKondratev <kondratev.an@inbox.ru>
2023-11-03 15:37:12 +0400

    Комбинированный компаратор

--- src/main/java/ru/job4j/collection/JobAscByName.java
+++ src/main/java/ru/job4j/collection/JobAscByName.java
@@ -6,6 +6,5 @@ public class JobAscByName implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
         return o1.getName().compareTo(o2.getName());
-
     }
 }
--- src/main/java/ru/job4j/collection/JobDescByPriority.java
+++ src/main/java/ru/job4j/collection/JobDescByPriority.java
@@ -7,6 +7,5 @@ public class JobDescByPriority implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
         return Integer.compare(o2.getPriority(), o1.getPriority());
-
     }
 }

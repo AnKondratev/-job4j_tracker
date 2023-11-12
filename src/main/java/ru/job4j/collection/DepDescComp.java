f121cfa774bd5cffcdd17ebb0cd83c83eaefc35e
AnKondratev <kondratev.an@inbox.ru>
2023-11-12 17:23:34 +0400

    Отсортировать департаменты

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,18 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] split1 = o1.split("/");
         String[] split2 = o2.split("/");
-        int res = -split1[0].compareTo(split2[0]);
-        if (res == 0) {
-            for (int i = 1; i < Math.min(split1.length, split2.length); i++) {
-                res = split1[i].compareTo(split2[i]);
-                if (res != 0) {
-                    break;
-                }
-            }
-            if (res == 0) {
-                res = Integer.compare(split1.length, split2.length);
-            }
-        }
-        return res;
+        int res = split2[0].compareTo(split1[0]);
+        return res != 0 ? res : o1.compareTo(o2);
     }
 }
